f857a5e3dfba4c7b904a1bca76cd917adfda13de
willhofner <59903484+willhofner@users.noreply.github.com>
2026-01-14 20:28:49 -0500

    Delete backend/__init__.py

--- backend/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-Fantasy Football Statistics Package
-"""
-from .season_analyzer import analyze_season
-from .wrapped_formatter import format_team_wrapped
-
-__all__ = ['analyze_season', 'format_team_wrapped']
