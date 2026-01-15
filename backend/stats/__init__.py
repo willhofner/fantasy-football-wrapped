26adee3a56965cf5cb858beeb4e6f66433f08785
willhofner <59903484+willhofner@users.noreply.github.com>
2026-01-14 20:27:47 -0500

    Create __init__.py

---
+++ backend/stats/__init__.py
@@ -0,0 +1,7 @@
+"""
+Fantasy Football Statistics Package
+"""
+from .season_analyzer import analyze_season
+from .wrapped_formatter import format_team_wrapped
+
+__all__ = ['analyze_season', 'format_team_wrapped']
